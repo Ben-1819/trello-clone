64e9f06a145ac476eb82486c8058bb2be240ab33
Ben-1819 <ben.brown20332@gmail.com>
2025-04-28 12:01:17 +0100

    feat: Added vue-router route
    Added vue-router route to show the Board component when the path is "/"

--- src/main.js
+++ src/main.js
@@ -5,8 +5,18 @@ import { createPinia } from 'pinia'
 import { createRouter, createWebHistory } from 'vue-router'
 import axios from 'axios'
 import App from './App.vue'
+import Board from './components/Board.vue'
 
 window.axios = axios;
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {path: "/", component: Board}
+    ],
+});
+
 createApp(App)
+.use(router)
 .use(createPinia())
 .mount('#app')
